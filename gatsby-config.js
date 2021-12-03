1454d3ae864950ca5fb17f64e2eb799adcd83499
Roger Guasch <roger.guasch.rion@gmail.com>
2021-12-03 18:25:18 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "RGR Gatsby v4",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
